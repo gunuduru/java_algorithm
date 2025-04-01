574ec5b3a3340a2a8921b2cb8c2f150471994810
gunuduru <goonoo0731@gmail.com>
2025-04-01 15:29:54 +0900

    Initial Commit

---
+++ src/programmers/Programmers42576.java
@@ -0,0 +1,8 @@
+package programmers;
+
+public class Programmers42576 {
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("java.version"));
+        System.out.println("Hello World");
+    }
+}
